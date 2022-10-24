43f47d104095fe907e6622ce3416c7a4f08eea5f
Fernando Pinto <fschultepinto@gmail.com>
2022-10-24 17:39:46 -0300

    criação do componente da section das notas

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,11 +1,19 @@
-import { Container } from './styles.js'
+import { Container, Links } from './styles.js'
 import { Button } from '../../components/button/index.jsx'
 import { Header } from '../../components/Header/index.jsx'
+import { Section } from '../../components/Section/index.jsx'
 
 export function Details() {
   return (
     <Container>
       <Header />
+
+      <Section title="Links úteis">
+        <Links>
+          <a href="#">Item 1</a>
+        </Links>
+      </Section>
+
       <Button title="Entrar"/>
     </Container>
   )
