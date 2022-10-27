708bcbcb8408ffa2229a9c0d7762d058585bdb7a
Fernando Pinto <fschultepinto@gmail.com>
2022-10-27 20:38:59 -0300

    Criação do buttontext e estilização da página

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,26 +1,37 @@
-import { Container, Links } from './styles.js'
+import { Container, Links, Content } from './styles.js'
 import { Button } from '../../components/button/index.jsx'
 import { Header } from '../../components/Header/index.jsx'
 import { Section } from '../../components/Section/index.jsx'
 import { Tag } from '../../components/Tag/index.jsx'
+import { ButtonText } from '../../components/ButtonText/index.jsx'
 
 export function Details() {
   return (
     <Container>
       <Header />
 
-      <Section title="Links úteis">
-        <Links>
-          <a href="#">Item 1</a>
-        </Links>
-      </Section>
+      <main>
+        <Content>
+          <ButtonText title="Excluir nota" />
 
-      <Section title="Marcadores">
-        <Tag title="express" />
-        <Tag title="nodejs" />
-      </Section>
+          <h1>Introdução ao React</h1>
+          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Qui animi inventore fuga quis! Molestias sapiente eaque labore ipsam aperiam? Quos totam itaque harum delectus natus eos minima quas vitae sint?</p>
 
-      <Button title="Entrar"/>
+          <Section title="Links úteis">
+            <Links>
+              <a href="#">Item 1</a>
+            </Links>
+          </Section>
+          
+          <Section title="Marcadores">
+            <Tag title="express" />
+            <Tag title="nodejs" />
+          </Section>
+          
+          <Button title="Entrar"/>
+        </Content>
+      </main>
+
     </Container>
   )
 }
