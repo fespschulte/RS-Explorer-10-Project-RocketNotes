15dc4dae0bb5235c5b49628db78619d752d19cd5
Fernando Pinto <fschultepinto@gmail.com>
2022-10-29 19:55:53 -0300

    Criação das rotas

--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -1,9 +1,41 @@
-import { Container } from "./styles";
+import { Textarea } from '../../components/TextArea';
+import { NoteItem } from '../../components/NoteItem';
+import { Section } from '../../components/Section';
+import { Button } from '../../components/Button';
+import { Header } from '../../components/Header'
+import { Input } from '../../components/Input'
+import { Container, Form } from "./styles";
+
 
 export function New() {
   return (
     <Container>
-      Teste
+      <Header />
+      <main>
+        <Form>
+          <header>
+            <h1>
+              Criar nota
+            </h1>
+            <a href="">voltar</a>
+          </header>
+          <Input placeholder="Título" />
+          <Textarea placeholder="Observações" />
+          <Section title="Links úteis">
+            <NoteItem value="https://rocketseat.com.br/"/>
+            <NoteItem isNew placeholder="Novo link"/>
+
+          </Section>
+          <Section title="Marcadores">
+            <div className="tags">
+              <NoteItem value="react"/>
+              <NoteItem isNew placeholder="Novo tag"/>
+            </div>
+
+          </Section>
+          <Button title="Salvar"/>
+        </Form>
+      </main>
     </Container>
   )
 }
