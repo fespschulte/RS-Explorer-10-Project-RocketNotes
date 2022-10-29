ff63d976151a7363446a273c3327256ef96eb6b5
Fernando Pinto <fschultepinto@gmail.com>
2022-10-28 22:10:17 -0300

    Criação da página Home

---
+++ src/pages/Home/styles.js
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  width: 100%;
+`
+export const Brand = styled.div`
+
+`
+export const Menu = styled.ul`
+
+`
+export const Search = styled.div`
+
+`
+export const Content = styled.div`
+
+`
+export const NewNot = styled.button`
+
+`
