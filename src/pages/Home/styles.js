026c2d22af3fe9723471826435a9e629920b6717
Fernando Pinto <fschultepinto@gmail.com>
2022-10-29 02:06:24 -0300

    Criação do componente das notas

--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -39,12 +39,14 @@ text-align: center;
 
 export const Search = styled.div`
 grid-area: search;
+padding: 64px 64px 0;
 
 `
 
 export const Content = styled.div`
 grid-area: content;
-background: yellow;
+padding: 0 64px;
+overflow-y: auto;
 
 `
 
