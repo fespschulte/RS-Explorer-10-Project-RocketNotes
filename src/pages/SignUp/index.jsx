391b5f735cebcca0d2042ab16338dc79b7f68b5f
Fernando Pinto <fschultepinto@gmail.com>
2022-10-29 18:36:16 -0300

    Criação das páginas de signin, signup, profile e new note

--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -1,24 +1,25 @@
 import { Background, Container, Form } from "./styles";
 import { Input } from "../../components/Input";
-import { FiLogIn, FiLock, FiMail } from "react-icons/fi";
+import { FiLogIn, FiLock, FiMail, FiUser } from "react-icons/fi";
 import { Button } from "../../components/button";
 import { ButtonText } from "../../components/ButtonText";
 
-export function SignIn() {
+export function SignUp() {
   return (
     <Container>
+      <Background />
       <Form>
         <h1>Rocket Notes</h1>
         <p>Aplicação para salvar e gerenciar seus links úteis</p>
-        <h2>Faça seu login</h2>
+        <h2>Crie sua conta</h2>
 
+        <Input placeholder="Nome" type="text" icon={FiUser}/>
         <Input placeholder="E-mail" type="text" icon={FiMail}/>
         <Input placeholder="Senha" type="password" icon={FiLock}/>
 
-        <Button title="Entrar"/>
-        <ButtonText title="Criar conta" isActive/>
+        <Button title="Cadastrar"/>
+        <ButtonText title="Voltar para o login" isActive/>
       </Form>
-      <Background />
     </Container>
   )
 }
